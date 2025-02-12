36e3add21abf811fd5c3ad623f0bb6ee2e1b0eaf
pawelgabrysiak <pawgab.05@gmail.com>
2025-02-12 20:25:22 +0100

    Update logout.php

--- cms/logout.php
+++ cms/logout.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-session_destroy();
-header("Location: login.php");
-exit();
+session_start(); // Rozpoczynanie sesji, aby uzyskać dostęp do istniejącej sesji użytkownika
+session_destroy(); // Usuwanie wszystkich danych sesji (zniszczenie) - użytkownik zostaje wylogowywany
+header("Location: login.php"); // Przekierowywanie użytkownika na stronę logowania
+exit(); // Zakończenie skryptu, aby upewnić się że dalszy kod nie zostanie wykonany
 ?>
