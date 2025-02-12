b3f20f47cd70692ea72861c8358a10ed2dc42d0c
pawelgabrysiak <pawgab.05@gmail.com>
2025-02-12 20:30:32 +0100

    Update test_pdo.php

--- cms/test_pdo.php
+++ cms/test_pdo.php
@@ -1,3 +1,5 @@
 <?php
-print_r(PDO::getAvailableDrivers());
+// Sprawdzanie dostępnych sterowników PDO na serwerze
+// PDO (PHP DATA OBJECTS) to interfejs umożliwiający komunikację z różnymi bazami danych
+print_r(PDO::getAvailableDrivers()); // Wyświetla listę dostępnych sterowników PDO (np. mysql, sqlite)
 ?>
